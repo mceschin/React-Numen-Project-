0ecdab0e24c0f798c072778432c8181558c07911
OknarfDev <contactopryra@gmail.com>
2022-08-18 10:37:12 -0300

    Borré archivos de sobra en el proyecto. En App.js agregué el Navbar de Martín, y las tarjetas con el botón de compras con los correspondientes archivos en las carpetas componentes, y stylesheets. FP

--- numen-proyect/src/App.js
+++ numen-proyect/src/App.js
@@ -1,17 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
-import card from 'react-bootstrap/';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Tarjeta from './componentes/card';
+import Tarjeta from './componentes/Tarjeta';
+// import Banner from './componentes/Banner';
+import NavbarComp from './componentes/NavbarComp'
+
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
+        <NavbarComp/>
+      </div>
+      {/* <Banner/> */}
       <div className='NuestrasOfertas'>
-      <Tarjeta></Tarjeta>
-      <Tarjeta></Tarjeta>
-      <Tarjeta></Tarjeta>
-      <Tarjeta></Tarjeta>
+        {/* De manera provisoria separé el título en dos h2 para poder usar colores distintos en "Nuestras" y "Ofertas". Falta chequear si hay una forma mejor de hacerlo. */}
+        <div className='titulo-contenedor'>
+          <h2 className='nuestras'>Nuestras</h2>
+          <h2 className='ofertas'>Ofertas</h2>
+        </div>
+        <div className='tarjetas'>
+          <Tarjeta 
+            imagen='02'
+            combo='Informal'
+            precio= '19.99'
+            precioanterior='24.99'/>
+          <Tarjeta 
+            imagen='03'
+            combo='Casual'
+            precio= '35.99'
+            precioanterior='44.99'/>
+          <Tarjeta 
+            imagen='04'
+            combo='Verano'
+            precio= '17.99'
+            precioanterior='20.99'/>
+          <Tarjeta 
+            imagen='05'
+            combo='Primavera'
+            precio= '24.99'
+            precioanterior='29.99'/>
+          
+        </div>
       </div>
     </div>
   );
